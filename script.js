8074dd4e251bbe0cb813535d2f48d6bcd4efa674
unknown <phat89116@gmail.com>
2025-07-08 14:33:13 +0700

    commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const title = document.getElementById("main-title");
+title.innerText = "Chào mừng bạn đến với khóa học JavaScript!";
+
+const desc = document.querySelector(".description");
+desc.innerHTML = "Bạn sẽ học về <strong>DOM, Events</strong>";
+
+function sayHello() {
+  const name = document.getElementById("student-name").value.trim();
+  const greeting = document.getElementById("greeting");
+  if (name) {
+    greeting.innerText = "Xin chào, ${name}!";
+  } else {
+    greeting.innerText = "Vui lòng nhập tên!";
+  }
+}
